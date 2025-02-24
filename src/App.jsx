6da0a68040ff648fa157583f93c955a7852e0c8c
Kleijn95 <kleijn95@gmail.com>
2025-02-24 11:57:04 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TopBar from "./components/TopBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TopBar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,54 @@
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { BellFill, ChatDotsFill, HouseFill, Linkedin, PeopleFill, SuitcaseLgFill } from "react-bootstrap-icons";
+
+function TopBar() {
+  return (
+    <Navbar bg="white">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Linkedin fill="blue" className="text-white fs-1" />
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Form className="d-flex">
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav.Link href="#home" className="d-flex flex-column align-items-center">
+            <HouseFill className="fs-5" fill="grey" />
+            Home
+          </Nav.Link>
+          <Nav.Link href="#home" className="d-flex flex-column align-items-center">
+            <PeopleFill className="fs-5" fill="grey" />
+            Rete
+          </Nav.Link>
+          <Nav.Link href="#home" className="d-flex flex-column align-items-center">
+            <SuitcaseLgFill className="fs-5" fill="grey" />
+            Lavoro
+          </Nav.Link>
+          <Nav.Link href="#home" className="d-flex flex-column align-items-center">
+            <ChatDotsFill className="fs-5" fill="grey" />
+            Messaggistica
+          </Nav.Link>
+          <Nav.Link href="#home" className="d-flex flex-column align-items-center">
+            <BellFill className="fs-5" fill="grey" />
+            Notifiche
+          </Nav.Link>
+          <NavDropdown title="Tu" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+          </NavDropdown>{" "}
+          <NavDropdown title="Per le Aziende" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopBar;
