fd19616d659f9bf9f9646b297c341ef6040e4b5c
francesco7773434 <francescoorbello5@gmail.com>
2025-02-25 12:54:51 +0100

    utenti

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import profileReducer from "../reducers/profileReducer";
 import asideReducer from "../reducers/asideReducer";
 import ExperienceReducer from "../reducers/experienceReducer";
+import utenteReducer from "../reducers/utenteReducer";
 
 const rootReducer = combineReducers({
   myprofile: profileReducer,
   aside: asideReducer,
   experience: ExperienceReducer,
+  utente: utenteReducer,
 });
 
 const store = configureStore({
