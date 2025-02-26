8d16047b064b7183a8d4fb83cc7d4048228ffb98
Zephyrior <mark.temblique@gmail.com>
2025-02-26 01:25:30 +0100

    pusho

--- src/components/ExperienceEditSection.jsx
+++ src/components/ExperienceEditSection.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { Button, Col, Container, Form, Image, Modal, Row, FormControl, FormGroup, FormLabel } from "react-bootstrap";
-import { PencilSquare, Plus } from "react-bootstrap-icons";
+import { ArrowLeft, PencilSquare, Plus } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchExperience, postExperience } from "../redux/actions/profileActions";
 import { useNavigate } from "react-router-dom";
 
-function Experience() {
+function ExperienceEditSection() {
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
@@ -39,16 +39,17 @@ function Experience() {
     dispatch(fetchExperience());
   }, [dispatch]);
   return (
-    <Container style={{ backgroundColor: "white" }} className="border rounded-3 mt-3 px-3 pt-3">
-      <div className="d-flex justify-content-between">
-        <h4>Experience</h4>
-
-        <div>
-          <Button variant="outline-*" className="p-0 mx-2" onClick={() => setShowModal(true)}>
-            <Plus className="fs-2" />
+    <Container style={{ backgroundColor: "white" }} className="border rounded-3 px-3 pt-3">
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          <Button variant="outline-*" className="mb-2 me-3 fs-3" onClick={() => navigate("/")}>
+            <ArrowLeft />
           </Button>
-          <Button variant="outline-*" className="pb-2 ms-2" onClick={() => navigate("/experience")}>
-            <PencilSquare className="fs-5" />
+          <h4>Experience</h4>
+        </div>
+        <div>
+          <Button variant="outline-*" className="mx-2" onClick={() => setShowModal(true)}>
+            <Plus className="fs-2" />
           </Button>
         </div>
       </div>
@@ -58,7 +59,7 @@ function Experience() {
             <Col xs="2">
               <Image fluid src={exp.image} />
             </Col>
-            <Col xs="10" className="ps-0">
+            <Col xs="9" className="ps-0">
               <h5>{exp.role}</h5>
               <p className="mb-0">{exp.company}</p>
               <p className="mb-0">
@@ -66,6 +67,9 @@ function Experience() {
               </p>
               <p className="mb-0">{exp.area}</p>
               <p className="mt-2">{exp.description}</p>
+            </Col>
+            <Col xs={1} className="pe-0">
+              <PencilSquare className="fs-5 mx-2" />
             </Col>
             <hr style={{ color: "gray" }} />
           </Row>
@@ -130,4 +134,4 @@ function Experience() {
   );
 }
 
-export default Experience;
+export default ExperienceEditSection;
