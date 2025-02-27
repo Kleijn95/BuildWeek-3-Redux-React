527d755a8a69cc1f92cacafc624ea257c57b9fc7
LeOMaSHI <LEONARDOMASHII@GMAIL.COM>
2025-02-27 23:21:25 +0100

    .

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -7,13 +7,11 @@ const Jobs = () => {
   return (
     <Container fluid className="mt-4 p-4 jobs-container">
       <Row className="g-4">
-        {/* Left Content - Visible only on lg screens and above */}
         <Col xs={12} lg={3} className="left-content d-none d-lg-block">
           <HomeProfile />
           <Card3Jobs />
         </Col>
-
-        {/* Right Content - Always visible on all screens */}
+        ={" "}
         <Col xs={12} lg={9} className="right-content">
           <JobsMain />
         </Col>
