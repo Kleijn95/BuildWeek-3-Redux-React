3ceb57a9612628bb8cab8a057af2e4570d0d9d4d
Zephyrior <mark.temblique@gmail.com>
2025-02-28 16:37:16 +0100

    pusho comment section

---
+++ src/redux/reducers/commentsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  content: [],
+};
+
+const commentsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_COMMENTS":
+      return { ...state, content: action.payload };
+    default:
+      return state;
+  }
+};
+export default commentsReducer;
